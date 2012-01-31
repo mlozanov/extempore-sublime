a2a616220783d96b01600ff27cdb4bd88a61aeba
mikhail lozanov <mlozanov@gmail.com>
2012-01-31 08:45:17 +0200

    some fixes, still do not know how to preserve connection state during the script life time

--- Extempore.py
+++ Extempore.py
@@ -1,6 +1,5 @@
 import sublime, sublime_plugin
-from socket import *
-
+import socket
 
 class ExtemporeConnection:
 	hostname = 'localhost'
@@ -9,17 +8,25 @@ class ExtemporeConnection:
 
 class ExtemporeConnectCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
-		ExtemporeConnection.s = socket(AF_INET, SOCK_STREAM)
-		ExtemporeConnection.s.connect((ExtemporeConnection.hostname, ExtemporeConnection.port))
+		try:
+			ExtemporeConnection.s = socket.socket(AF_INET, SOCK_STREAM)
+			ExtemporeConnection.s.settimeout(5)
+			ExtemporeConnection.s.connect((ExtemporeConnection.hostname, ExtemporeConnection.port))
+			data = ExtemporeConnection.s.recv(1024)
+			sublime.status_message(data)
+		except socket.error, e:
+			return None
 
 class ExtemporeSendCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		#for sel in self.view.sel():
-		#	theString = self.view.substr(sel)
-		ExtemporeConnection.s.send('(define a (lambda (x) (+ x x)))')
-		data = ExtemporeConnection.s.recv(1024)
+		for sel in self.view.sel():
+			theCode = self.view.substr(sel)
 
-			#print repr(data)
-		sublime.error_message(data)
+			try:
+				ExtemporeConnection.s.send(theCode)
+				data = ExtemporeConnection.s.recv(1024)
+				sublime.status_message(data)
+			except socket.error, e:
+				sublime.status_message(e)
 
 		self.view.sel().clear()
